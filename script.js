3395b36e0cf4ff1c724103e36beb62120db998b0
jacob <873318@pdsb.net>
2026-01-26 18:24:59 -0500

    Title page is nearly complete.

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
+const hero = document.querySelector('.hero');
+const heroImage = document.querySelector('.hero-image');
 
-// animation trigger
-const observerOptions = {
-    threshold: 0.2
-};
+// split headings
+// H1 (split by <br>)
+document.querySelectorAll('.split-heading').forEach(h1 => {
+  const parts = h1.innerHTML.split('<br>');
+  h1.innerHTML = parts
+    .map(line => `<span class="line">${line}</span>`)
+    .join('');
+});
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.querySelector('.hero-content').classList.add('active');
-            entry.target.querySelector('.hero-visual').classList.add('active');
-        }
-    });
-}, observerOptions);
+// H2 (single line)
+document.querySelectorAll('.split-subheading').forEach(h2 => {
+  h2.innerHTML = `<span class="line">${h2.textContent}</span>`;
+});
 
-observer.observe(document.querySelector('.hero'));
+// split paragraph
+document.querySelectorAll('.hero-content p').forEach(p => {
+  p.innerHTML = p.innerHTML
+    .split('\n\n') // double line breaks
+    .map(line => `<span class="line paragraph">${line}</span>`)
+    .join('')
+});
+
+// observer
+const lines = hero.querySelectorAll('.line');
+
+const heroObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+
+      // trigger text immediately
+      lines.forEach((line, i) => {
+        setTimeout(() => {
+          line.classList.add('active');
+        }, i * 100);
+      });
+
+      // trigger image shortly after section enters
+      setTimeout(() => {
+        const heroImages = document.querySelectorAll('.hero-visual');
+        heroImages.forEach((img, i) => {
+            setTimeout(() => {
+                img.classList.add('active');
+            }, i * 200);
+        });
+      }, 200);
+
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+heroObserver.observe(hero);
